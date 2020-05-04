cbbb5fa5ac6c90c407b5575cfbb786ac4dae771a
Lukas Panni <lukas.panni@outlook.de>
2020-05-04 09:00:41 +0200

    Code Documentation

--- grouper.py
+++ grouper.py
@@ -4,6 +4,9 @@ from time import localtime
 
 
 class GroupBy(Enum):
+    """
+    Possible groups.
+    """
     YEAR = 1
     MONTH = 2
     MONTH_NAMED = 3
@@ -11,6 +14,9 @@ class GroupBy(Enum):
 
 
 class Grouper:
+    """
+    Used to group images by creation time.
+    """
     month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
                    "November", "December"]
 
@@ -18,6 +24,13 @@ class Grouper:
         self.group_list = group_list
 
     def get_group_directory(self, image: ImageFile) -> str:
+        """
+        Get the group directory name (based on creation time and configured groups) of a given image.
+        :param image: image file
+        :return: name of the group directory
+        """
+        if len(self.group_list) < 1:
+            return ""
         group_directory = ""
         creation_time = image.get_creation_time()
         if GroupBy.YEAR in self.group_list:
--- image_copier.py
+++ image_copier.py
@@ -7,19 +7,32 @@ from image_file import ImageFile
 
 
 class ImageCopier:
+    """
+    Used to copy images  from one location to another.
+    """
     def __init__(self, output_dir: str, grouping: Optional[Grouper] = None):
         self.output_dir = output_dir
         self.grouping = grouping
         if not os.path.exists(self.output_dir):
             os.mkdir(self.output_dir)
 
-    def get_destination_directory(self, image):
+    def get_destination_directory(self, image) -> str:
+        """
+        Get the destination directory for a given image.
+        The destination already includes grouping.
+        :param image: image file
+        :return: destination directory
+        """
         destination = self.output_dir
         if self.grouping is not None:
             destination += self.grouping.get_group_directory(image)
         return destination
 
     def copy(self, image: ImageFile):
+        """
+        Copy the image to its new location.
+        :param image: image file to copy.
+        """
         destination_dir = self.get_destination_directory(image)
         if not os.path.exists(destination_dir):
             os.makedirs(destination_dir, exist_ok=True)
@@ -27,6 +40,9 @@ class ImageCopier:
 
 
 class RAWSeparateImageCopier(ImageCopier):
+    """
+    Used to copy images and separate RAW and JPG images during copy.
+    """
     def __init__(self, output_dir: str, raw_dir_name: str, grouping: Optional[Grouper] = None):
         super().__init__(output_dir, grouping)
         self.raw_dir_name = raw_dir_name
