53b277cb650cd1b8ad7cf09287c4a916daffe5bf
Lukas Panni <lukas.panni@outlook.de>
2020-07-16 10:20:03 +0200

    Fix: pytest not finding tests

--- tests/test_RawSeparateTransform.py
+++ tests/test_RawSeparateTransform.py
@@ -1,21 +1,17 @@
-import unittest
 import os
 from pathlib import Path
+
 from ImageCopy.RawSeparateTransform import RawSeparateTransform
 from ImageCopy.image_file import ImageFile
 
 
-class RawSeparateTransformTest(unittest.TestCase):
+class TestRawSeparateTransform:
     transform = RawSeparateTransform("RAW")
 
-    def test_separate_raw(self):
+    def test_transform(self):
         test_dict = {ImageFile(Path("/lol/img.raw"), ".raw"): "/output/",
                      ImageFile(Path("/lol/img.jmg"), ".jpg"): "/output/"}
         expected_output = [os.path.join("/output/", "RAW"), "/output/"]
         self.transform.transform(test_dict)
         actual_output = [v for v in test_dict.values()]
-        self.assertEqual(expected_output, actual_output)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert expected_output == actual_output
