1a1430673c32aaf5d4d1ce8d32252df4ceba63e7
Lukas Panni <lukas-panni@web.de>
2020-08-02 19:49:45 +0200

    Fix Test

--- tests/test_RawSeparateTransform.py
+++ tests/test_RawSeparateTransform.py
@@ -11,7 +11,7 @@ class TestRawSeparateTransform:
     def test_transform(self):
         imgs = [ImageFile(Path("/lol/img.raw"), ".raw"), ImageFile(Path("/lol/img.jmg"), ".jpg")]
         test_dict = {img: "/output/" for img in imgs}
-        expected_output = [os.path.join("/output/", "RAW"), "/output/"]
+        expected_output = [os.path.join("/output/", "RAW/"), "/output/"]
         self.transform.transform(test_dict)
         actual_output = [v for v in test_dict.values()]
         assert actual_output == expected_output
