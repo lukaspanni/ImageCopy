d60ecc6eaf10c06547b4dc926620761b279f06e8
Lukas Panni <lukas.panni@outlook.de>
2021-12-09 18:47:18 +0100

    Fix copy without override

--- ImageCopy/image_file.py
+++ ImageCopy/image_file.py
@@ -46,6 +46,9 @@ class ImageFile:
                 creation_time = stat.st_mtime
         return time.localtime(creation_time)
 
+    def get_filename(self) -> str:
+        return os.path.basename(self.path)
+
     def __str__(self) -> str:
         return str(self.path)
 
